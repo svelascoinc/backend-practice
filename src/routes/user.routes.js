8cb5902cff02e445136242b36dfc4207353466bc
Samuel V <ext_cleverit@falabella.cl>
2020-04-17 14:55:19 -0400

    ADD: Cache added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,6 @@
 const { Router} = require("express");
-const { AuthMiddleware, ParseIntMiddleware } = require("../middlewares")
+const { AuthMiddleware, ParseIntMiddleware, CacheMiddleware } = require("../middlewares")
+const { CACHE_TIME } = require("../helpers");
 
 module.exports = function({ UserController }) {
     const router = Router();
@@ -7,7 +8,7 @@ module.exports = function({ UserController }) {
 /* Cuando se invoque un llamado a la ruta Home, 
 El controlador HomeController se ejecutará con el metodo Index()*/
     router.get("/:userId", UserController.get);
-    router.get("", [AuthMiddleware, ParseIntMiddleware], UserController.getAll);
+    router.get("", [AuthMiddleware, ParseIntMiddleware, CacheMiddleware(CACHE_TIME.ONE_HOUR)], UserController.getAll);
     router.patch("/:userId", UserController.update);
     router.delete("/:userId", UserController.delete);
 
